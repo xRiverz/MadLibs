78515da031c2b08787975284b697f4e932fce9f5
xRiverz <anhargalam@gmail.com>
2021-12-23 12:52:40 +0000

    00

---
+++ MadLibs/TextViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TextViewController.swift
+//  MadLibs
+//
+//  Created by admin on 23/12/2021.
+//
+
+import UIKit
+
+class TextViewController : UIViewController {
+    
+    @IBOutlet weak var adjField : UITextField!
+    @IBOutlet weak var v1Field : UITextField!
+    @IBOutlet weak var v2Field : UITextField!
+    @IBOutlet weak var nounField : UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- MadLibs/ViewController.swift
+++ MadLibs/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var statmentLable : UILabel!
+    
+    var Text : [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func unwindtomain(_ unwindSegue : UIStoryboardSegue){
+        if unwindSegue.source is TextViewController {
+            let senderVC = unwindSegue.source as! TextViewController
+            statmentLable.text = "We are having a perfectly \(senderVC.adjField.text!) right now. Later we will \(senderVC.v1Field.text!) and \(senderVC.v2Field.text!) in the \(senderVC.nounField.text!)"
+        }
+    }
 
 }
 
